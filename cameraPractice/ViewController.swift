7efb6936774dafcdef0c9f473d0dda9e2931f03c
kaylasgoldstein <kaylasgoldstein@gmail.com>
2019-06-24 16:39:02 -0400

    first draft

--- cameraPractice/ViewController.swift
+++ cameraPractice/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var newScreen = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        newScreen.delegate = self
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func takePhotoTapped(_ sender: UIButton) {
+        newScreen.sourceType = .camera
+        present(newScreen, animated: true, completion: nil)
+    }
+    
+    
+    @IBOutlet weak var photoTaken: UIImageView!
+    
+    //our last step
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            photoTaken.image = selectedImage
+        }
+        newScreen.dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
 
